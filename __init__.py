437209a053ca171eb9bee1bddaa6d097f28f38d0
Internetimagery <jason.dixon.email@gmail.com>
2015-09-23 16:07:43 +1200

    adding frame range as default

--- __init__.py
+++ __init__.py
@@ -6,3 +6,37 @@
 # store animation layer information, turned on or off, per anim
 # store frame information per animaton
 # store rig information (only once?)
+
+import maya.cmds as cmds
+from json import loads, dumps
+
+class Store(object):
+    """
+    Store metadata
+    """
+    def __init__(s):
+        s.dataName = "GameAnimExport"
+        try:
+            s.data = loads(cmds.fileInfo(s.dataName, q=True)[0].decode("unicode_escape"))
+        except ValueError, IndexError:
+            s.data = {}
+    def get(s, k, default=None):
+        return s.data.get(k, default)
+    def set(s, k, v):
+        s.data[k] = v
+        cmds.fileInfo(k, dumps(s.data))
+
+class AnimationCreate(object):
+    """
+    Window to create or edit an animation entry
+    """
+    def __init__(s, override={}):
+        s.data = {
+            "range" : s.frameRange(),
+            "layers": {}
+        }
+    def frameRange(s):
+        return [
+            cmds.playbackOptions(q=True, min=True),
+            cmds.playbackOptions(q=True, max=True)
+        ]
