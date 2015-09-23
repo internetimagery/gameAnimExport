cb1988b7fff50096068538a45d97995c8652fc6d
Internetimagery <jason.dixon.email@gmail.com>
2015-09-23 15:48:44 +1200

    adding some files

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# TODO
+
+# Save scene information json into fileInfo
+# toolbox for viewing information
+# seperate window for adding information
+# store animation layer information, turned on or off, per anim
+# store frame information per animaton
+# store rig information (only once?)
